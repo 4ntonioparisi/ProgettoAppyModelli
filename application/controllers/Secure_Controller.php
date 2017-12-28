6b9ac7e4e5646dee48e92f8affbcae04cb093277
gveriello <gveriello@outlook.it>
2017-12-28 16:20:06 +0100

    Maximum allowed number of methods - test - Giuseppe

--- application/controllers/Secure_Controller.php
+++ application/controllers/Secure_Controller.php
@@ -53,49 +53,6 @@ class Secure_Controller extends CI_Controller
 		}
 	}
 
-	protected function track_page($path, $page)
-	{
-		if(get_instance()->Appconfig->get('statistics'))
-		{
-			$this->load->library('tracking_lib');
-
-			if(empty($path))
-			{
-				$path = 'home';
-				$page = 'home';
-			}
-
-			$this->tracking_lib->track_page('controller/' . $path, $page);
-		}
-	}
-
-	protected function track_event($category, $action, $label, $value = NULL)
-	{
-		if(get_instance()->Appconfig->get('statistics'))
-		{
-			$this->load->library('tracking_lib');
-
-			$this->tracking_lib->track_event($category, $action, $label, $value);
-		}
-	}
-
-	public function numeric($str)
-	{
-		return parse_decimals($str);
-	}
-
-	public function check_numeric()
-	{
-		$result = TRUE;
-
-		foreach($this->input->get() as $str)
-		{
-			$result = parse_decimals($str);
-		}
-
-		echo $result !== FALSE ? 'true' : 'false';
-	}
-
 
 	// this is the basic set of methods most OSPOS Controllers will implement
 	public function index() { return FALSE; }
