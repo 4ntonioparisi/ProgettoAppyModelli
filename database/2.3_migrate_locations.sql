19ec61265d1cc659a4e7aaba4d3815ad0ee753a3
gveriello <gveriello@outlook.it>
2017-12-28 15:12:51 +0100

    Avoid repeating the same query code - Giuseppe
    database/2.3_migrate_locations.sql

--- database/2.3_migrate_locations.sql
+++ database/2.3_migrate_locations.sql
@@ -1,3 +1,3 @@
-
+ 
 INSERT INTO ospos_item_quantities (item_id, location_id, quantity) (SELECT item_id, location_id, quantity FROM ospos_items oi, ospos_stock_locations osl where oi.location = osl.location_name);
 ALTER TABLE ospos_items DROP COLUMN location;
